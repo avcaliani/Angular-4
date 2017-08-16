13b5fe0c1477a99116e5eb6449cfe4d0199523f1
Anthony Vilarim Caliani <avcaliani.it@gmail.com>
2017-08-16 14:55:50 -0300

    Atualizando os diretórios.

--- skeleton/src/app/app.module.ts
+++ skeleton/src/app/app.module.ts
@@ -8,9 +8,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FormComponent } from './pages/form/form.component';
 import { Error404Component } from './pages/error-404/error-404.component';
-import { PipesComponent } from './includes/pipes/pipes.component';
-import { SortPipe } from './includes/pipes/pipes.sort';
-import { ListComponent } from './includes/list/list.component';
+import { PipesComponent } from './pages/includes/pipes/pipes.component';
+import { SortPipe } from './pages/includes/pipes/pipes.sort';
+import { ListComponent } from './pages/includes/list/list.component';
 
 @NgModule({
   declarations: [
