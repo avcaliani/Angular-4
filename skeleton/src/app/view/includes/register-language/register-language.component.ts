44f6582dccc691d4d18d5dc3d37c0659dbebd6ef
Anthony Vilarim Caliani <avcaliani.it@gmail.com>
2017-09-08 00:15:36 -0300

    Adicionando exemplo de vadição de Formulário.

--- skeleton/src/app/view/includes/register-language/register-language.component.ts
+++ skeleton/src/app/view/includes/register-language/register-language.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-language',
@@ -15,9 +15,24 @@ export class RegisterLanguageComponent implements OnInit {
   ngOnInit() {
 
     this.langForm = new FormGroup({
-      langName: new FormControl("java")
+      // This way just check if field is not empty.
+      // langName: new FormControl("", Validators.required)
+      // And this is a little bit diffferent
+      // langName: new FormControl("", this.textValidator)
+      // And you can use this one too
+      langName: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(4),
+        Validators.pattern("Java|JAVA|java") // RegExr HueHue
+      ]))
     });
 
+  }
+
+  textValidator = function(control) {
+    if (control.value.length < 4) {
+      return {'langName': true};
+    }
   }
 
   onSubmit = function (language) {
