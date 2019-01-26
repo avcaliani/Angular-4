1b534f8c874c9d78b6c1db19f71329eb9154ff15
Anthony <avcaliani.it@gmail.com>
2019-01-25 23:26:04 -0200

    Adding PWA Module.

--- service-worker/src/app/app.module.ts
+++ service-worker/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post/post-list/post-list.component';
 import { PostItemComponent } from './post/post-item/post-item.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { PostItemComponent } from './post/post-item/post-item.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
